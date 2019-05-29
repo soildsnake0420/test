0090b56c20111ec90507173ef9e7682f011956b5
soildsnake0420 <49671317+soildsnake0420@users.noreply.github.com>
2019-05-29 17:59:12 +0800

    Update gevent_bs4.py
    面向对象的方法，加入了 queue

--- gevent_bs4.py
+++ gevent_bs4.py
@@ -4,10 +4,6 @@ from bs4 import BeautifulSoup
 import gevent, requests
 from gevent.queue import Queue
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
-}
-
 url_list = [
             'http://www.mtime.com/top/tv/top100/',
             'http://www.mtime.com/top/tv/top100/index-1.html',
@@ -22,69 +18,72 @@ url_list = [
             'http://www.mtime.com/top/tv/top100/index-10.html'
             ]
 
-#创建队列对象，赋值给work。 Queue本身为不限数量的队列，如果Queue(5)则最多容纳5个任务超出的话会报错，0和不输入是一个意义
-work = Queue()
+class gevent_movies():
 
-for url in url_list:
-    #将网址都存入队列
-    try:
-        work.put_nowait(url)
-    except:
-        print('队列满了')
+    def __init__(self, url_list):
 
-#执行方法
-def crawler(url):
+        # 任务列表
+        self.tasks_list = []
 
-    #当队列不为空则执行
-    while not work.empty():
+        # 把网址列表遍历
+        for url in url_list:
+            # 创建一个任务，执行 上面的方法
+            task = gevent.spawn(self.crawler(url))
 
-        #取出网址
-        url = work.get_nowait()
+            # 把创建好的任务放进任务列表
+            self.tasks_list.append(task)
 
-        #开始抓取数据
-        r = requests.get(url, headers= headers)
-
-        bs = BeautifulSoup(r.text, 'html.parser')
-
-        #电影内容
-        bingcenters = bs.find_all(class_='mov_con')
+        # 开始执行任务
+        gevent.joinall(self.tasks_list)
 
 
-        for bingcenter in bingcenters:
+    #执行方法
+    def crawler(self, urls):
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+
+        work = Queue()
+        for url in url_list:
+            work.put_nowait(url)
+
+        #当队列不为空则执行
+        while not work.empty():
+
+            #取出网址
+            url = work.get_nowait()
+
+            #开始抓取数据
+            r = requests.get(urls, headers = headers)
+
+            bs = BeautifulSoup(r.text, 'html.parser')
+
+            #电影内容
+            bingcenters = bs.find_all(class_='mov_con')
 
 
-            try:
-                #电影标题
-                title = bingcenter.find('h2')
-                #电影导演
-                dirctor = bingcenter.find_all('p')[0]
-                #电影主演
-                actor = bingcenter.find_all('p')[1]
-                #电影简介
-                content = bingcenter.find(class_='mt3')
+            for bingcenter in bingcenters:
 
-                #打印出这些信息
-                print(title.text)
-                print(dirctor.text)
-                print(actor.text)
-                print(content.text)
-            except:
-                #如果缺失信息，则打印下面
-                print('暂无信息')
 
-            print('---------------')
+                try:
+                    #电影标题
+                    title = bingcenter.find('h2')
+                    #电影导演
+                    dirctor = bingcenter.find_all('p')[0]
+                    #电影主演
+                    actor = bingcenter.find_all('p')[1]
+                    #电影简介
+                    content = bingcenter.find(class_='mt3')
 
-#任务列表
-tasks_list = []
+                    #打印出这些信息
+                    print(title.text)
+                    print(dirctor.text)
+                    print(actor.text)
+                    print(content.text)
+                except:
+                    #如果缺失信息，则打印下面
+                    print('暂无信息')
 
-#把网址列表遍历
-for url in url_list:
+                print('---------------')
 
-    #创建一个任务，执行 上面的方法
-    task = gevent.spawn(crawler(url))
-
-    #把创建好的任务放进任务列表
-    tasks_list.append(task)
-
-#开始执行任务
-gevent.joinall(tasks_list)
+news = gevent_movies(url_list)
